1303e9337457cb978cee7e3435ebf09b85e6098e
Apple <apple@Applede-MacBook-Pro.local>
2019-08-27 18:17:17 +0800

    api health check

---
+++ p005api/src/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r:= gin.Default()
+
+
+	api := r.Group("/api")
+	{
+		// Health Check
+		api.GET("/health", func(context *gin.Context) {
+			context.JSON(200,gin.H{
+				"message": "Alive",
+			})
+		})
+
+		v1 := api.Group("/v1")
+		{
+			v1.POST("/user/login")
+		}
+	}
+	r.Run(":8080")
+
+}
