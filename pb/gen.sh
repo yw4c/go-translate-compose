cfbe56183796d6d03cd55f42cc232e911d1123ef
yw4c <yw4code@gmail.com>
2019-12-03 19:13:28 +0800

    update services

--- pb/gen.sh
+++ pb/gen.sh
@@ -1,2 +1,3 @@
-protoc -I ./ --go_out=plugins=grpc:. ./p010user/v1/user.proto
-protoc -I ./ --go_out=plugins=grpc:. ./p012dict/v1/dict.proto
+protoc -I ./ --go_out=plugins=grpc:. ./github.com/yw4c/trans-pb/p010user/v1/user.proto
+protoc -I ./ --go_out=plugins=grpc:. ./github.com/yw4c/trans-pb/p012dict/v1/dict.proto
+protoc -I ./ --go_out=plugins=grpc:. ./github.com/yw4c/trans-pb/p013keep/v1/keep.proto
